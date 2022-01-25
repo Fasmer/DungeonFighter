cffcbb3714c2ba898c2485b21febc8d20320487e
MaxKhomutov <58793124+Fasmer@users.noreply.github.com>
2022-01-25 15:17:45 +0300

    Движение персонажа
    Добавлено движение персонажа и коллизия с другими объектами

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class Player : MonoBehaviour
 {
+    private BoxCollider2D boxCollider;
+    private Vector3 moveDelta;
+    private RaycastHit2D hit;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        //Reset moveDelta
+        moveDelta = new Vector3(x, y, 0);
+
+        //Swap sprite dirrection left and right
+
+        if (moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if (moveDelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        // Make sure we can moove in this direction, by casting aba there first, if the box returns null, we're free to move
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            //Move player
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            //Move player
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
+
     }
 }
